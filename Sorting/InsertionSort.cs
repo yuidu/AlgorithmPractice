dccf5f37c98354c6a6927508ee19290d3acfccd6
Yu Eric <yuiduiyu@gmail.com>
2019-02-21 21:39:36 +0800

    Put sorting algorithm under a seperate folder

--- Sorting/InsertionSort.cs
+++ Sorting/InsertionSort.cs
@@ -23,6 +23,9 @@ namespace AlgorithmPractice
         
         //This version of insertion sort has better performance, which is better than selection sort since
         //it only compares partial elements and it doesn't have swap operation that takes longer time.
+        //if the array is almost sorted, then this insertion sort has good performance since it will break ealier
+        //for almost sorted array, insertion sort is even faster than O(nlogn) sorting algorithm.
+        //if the array is already sorted, insertion sort is O(n), because inner loop return immediately.
         public void Sort2(List<int> arr)
         {
             for(int i=1; i<arr.Count; i++)
